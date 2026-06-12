d5970f91dc6ba10f04e61dcb4b0cf51010a10e89
AGX-Servers <medogxc6@gmail.com>
2026-06-12 21:30:23 +0300

    Fix websockify command and update network options

--- agx-server-qemu-setup_win_server2022_vhd.sh
+++ agx-server-qemu-setup_win_server2022_vhd.sh
@@ -2,7 +2,7 @@
 sleep 3s;
 clear;
 sudo apt-get update && sudo apt-get install -y novnc python3-websockify;
-websockify --web=/usr/share/novnc/ 6080 localhost:5901;
+websockify --web=/usr/share/novnc/ 6080 localhost:5901 &
 sleep 2s;
 sudo apt update; sudo apt upgrade -y; sudo apt install git wget qemu-system-x86 ovmf -y ; clear; wget -O win.vhd https://software-download.microsoft.com/download/pr/20348.169.amd64fre.fe_release_svc_refresh.210806-2348_server_serverdatacentereval_en-us.vhd;
 clear;
@@ -17,4 +17,4 @@ qemu-system-x86_64 \
   -vga std \
   -display vnc=:1 \
   -boot c \
-  -net nic -net user
+  -net nic -net user &
