d17abf7148594b79a29be5bca564cc388a8b4cd9
AGX-Servers <medogxc6@gmail.com>
2026-06-12 21:26:53 +0300

    Refactor agx-server-qemu-setup script for clarity

--- agx-server-qemu-setup_win_server2022_vhd.sh
+++ agx-server-qemu-setup_win_server2022_vhd.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-sleep 3s
+sleep 3s;
 clear;
-sudo apt-get update && sudo apt-get install -y novnc python3-websockify
-websockify --web=/usr/share/novnc/ 6080 localhost:5901
-sleep 2s
+sudo apt-get update && sudo apt-get install -y novnc python3-websockify;
+websockify --web=/usr/share/novnc/ 6080 localhost:5901;
+sleep 2s;
 sudo apt update; sudo apt upgrade -y; sudo apt install git wget qemu-system-x86 ovmf -y ; clear; wget -O win.vhd https://software-download.microsoft.com/download/pr/20348.169.amd64fre.fe_release_svc_refresh.210806-2348_server_serverdatacentereval_en-us.vhd;
 clear;
+sleep 2s;
+echo "Server Started Successfully (^_*) ";
 qemu-system-x86_64 \
   -m 10G \
   -hda win.vhd \
